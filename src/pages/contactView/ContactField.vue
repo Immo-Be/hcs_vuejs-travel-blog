e3fffab158fe1947a070a8727327ae5df94673dd
Immo-Be <i.beeck@gmail.com>
2022-03-30 19:28:54 +0200

    updated contact text

--- src/pages/contactView/ContactField.vue
+++ src/pages/contactView/ContactField.vue
@@ -7,12 +7,14 @@
       My name is Immo and I am an marketeer gone coding. This travel blog is
       part of my final project at the
       <strong>Hamburg Coding School.</strong> There is a lot to see and do so
-      feel free to play around with the features 😉
+      play around with the features
     </h2>
     <h2 class="subtitle">
       This website is built using Vue.js as a javascript framework. It also
-      takes advantage of the power of the Bulma CSS framework. If you have any
-      inquires, comments or just want to hire me, feel free to use the
+      takes advantage of the power of the Bulma CSS framework. The backend
+      powered by node.js is hosted on heroku and is connected to a mySQL
+      database. So feel free to CRUD all trips 😉. If you have any inquires,
+      comments or just want to hire me, feel free to use the
       <strong>form below</strong> (you can literally do so, it literally works
       for real... no drill)
     </h2>
